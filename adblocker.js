32c15fc02e6cc487c8b2196c3caf905378974f09
scriptblog <91836339+scriptblog@users.noreply.github.com>
2021-10-23 05:23:16 +0700

    Update adblocker.js

--- adblocker.js
+++ adblocker.js
@@ -2,3 +2,4 @@ var e=document.createElement('div');
 e.id='TAiBOSegoXEu';
 e.style.display='none';
 document.body.appendChild(e);
+function downloadJSAtOnload(){var d=document.createElement("script");d.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
